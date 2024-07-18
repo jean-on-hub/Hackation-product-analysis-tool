7fb096ca295423df7c4e51869c32f58264ac2032
jean-on-hub <87473040+jean-on-hub@users.noreply.github.com>
2024-07-18 15:32:05 +0000

    Revert "push"
    This reverts commit dd8ac300edb8ed8f0e810b8fe142f91507594130.

--- streaml.py
+++ streaml.py
@@ -6,132 +6,76 @@ from functions import *
 from langchain_community.chat_models import ChatOpenAI
 from langchain_experimental.agents.agent_toolkits.pandas.base import create_pandas_dataframe_agent
 from streamlit_chat import message
+from streamlit_image_select import image_select
+# from langchain.llms import OpenAI
 
 # Load environment variables from .env
-load_dotenv()
+# load_dotenv()
 
 def get_text(n):
-    input_text = st.text_input('Type your question here.', '', key="input{}".format(n), on_change=submit_question, args=(n,))
-    return input_text
-
-def format_sales_figures(df):
-    # Assuming the column to format is named 'sales'
-    if 'sales' in df.columns:
-        df['sales'] = df['sales'].apply(lambda x: "{:,.2f}".format(x))
-    return df
-
-def load_csv_with_error_handling(file):
-    encodings = ['utf-8', 'latin1', 'ISO-8859-1']
-    for encoding in encodings:
-        try:
-            df = pd.read_csv(file, encoding=encoding)
-            return df
-        except Exception as e:
-            continue
-    st.error(f"Failed to read the file {file.name} with the available encodings.")
-    return None
-
-def submit_question(n):
-    if st.session_state.get(f'input{n}', ''):
-        st.session_state['user_input'] = st.session_state[f'input{n}']
-        st.session_state[f'input{n}'] = ''
-
+    input_text= st.text_input('How can I help?', '', key="input{}".format(n))
+    return input_text 
 def main():
-    # Access your API key
+# Access your API key
     api_key = os.getenv('OPENAI_API_KEY')
 
-    if not api_key:
-        st.error("OpenAI API key is missing. Please set it in the .env file.")
-        return
+    # # Print the API key once to check if it's correctly loaded
+    # if api_key:
+    #     print(f"Loaded API key: {api_key}")
+    # else:
+    #     print("Failed to load API key.")
+    # dotenv_path = find_dotenv()
+    # if dotenv_path:
+    #     load_dotenv(dotenv_path, override=True)
 
-    # Load the preloaded dataset
-    preloaded_iris = pd.read_excel('Dummy Dataset for Challenge #1.xlsx', 'Database')
-    preloaded_iris = format_sales_figures(preloaded_iris)
-
-    # Set up the Streamlit app
-    st.title('Welcome to ChatInsights')
-    st.write("Ask a question to the chatbot based on the dummy dataset provided for the hackathon.")
-
-    # Explain subscription tiers
-    st.sidebar.title("Subscription Tiers")
-    st.sidebar.write("### Free Tier:")
-    st.sidebar.write("- Access to basic chatbot features")
-    st.sidebar.write("- Display of dataframes")
-    st.sidebar.write("### Pro Tier:")
-    st.sidebar.write("- Access to all Free Tier features")
-    st.sidebar.write("- Display of generated graphs and plots")
-    st.sidebar.write("### Premium Tier:")
-    st.sidebar.write("- Access to all Pro Tier features")
-    st.sidebar.write("- Ability to upload your own datasets in place of the preloaded dataset")
-
-    # Subscription selection
-    subscription_tier = st.sidebar.selectbox("Select your subscription tier:", ("Free", "Pro", "Premium"))
-
-    # User input
-    x = 0
-    user_input = get_text(x)
-
-    user_uploaded_dfs = []
-    if subscription_tier == "Premium":
-        st.title("Upload your datasets")
-        uploaded_files = st.file_uploader("Choose files", type=["csv", "xlsx", "xls"], accept_multiple_files=True)
-        for uploaded_file in uploaded_files:
-            try:
-                if uploaded_file.name.endswith(".csv"):
-                    df = load_csv_with_error_handling(uploaded_file)
-                else:
-                    df = pd.read_excel(uploaded_file)
-                if df is not None:
-                    df = format_sales_figures(df)
-                    user_uploaded_dfs.append(df)
-            except Exception as e:
-                st.error(f"An unexpected error occurred with file {uploaded_file.name}: {e}")
-                return
-
-    # Use user-uploaded datasets if available, otherwise use preloaded dataset
-    if user_uploaded_dfs:
-        combined_df = pd.concat(user_uploaded_dfs, ignore_index=True)
-    else:
-        combined_df = preloaded_iris
+    print(os.getenv('MY_API_KEY'))
+    # Load the dataset
+    iris = pd.read_excel('Dummy Dataset for Challenge #1.xlsx', 'Database')
 
     # Create the chatbot agent
     chat = ChatOpenAI(openai_api_key=api_key, model_name='gpt-3.5-turbo', temperature=0.0)
-    agent = create_pandas_dataframe_agent(chat, combined_df, return_intermediate_steps=True, verbose=True, allow_dangerous_code=True)
-
-    if 'user_input' in st.session_state:
-        user_input = st.session_state['user_input']
-        if user_input:
-            try:
-                response, thought, action, action_input, observation, plot_objects, dataframe_objects = run_query(agent, user_input)
-                st.session_state.past.append(user_input)
-                st.session_state.generated.append(response)
-                st.session_state.plots.append(plot_objects)
-                st.session_state.dataframes.append(dataframe_objects)
-
-                for i in range(len(st.session_state['generated']) - 1, -1, -1):
-                    message(st.session_state["generated"][i], key=str(i))
-                    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-
-                    if subscription_tier in ["Pro", "Premium"]:
-                        if st.session_state['plots'][i]:
-                            for plot in st.session_state['plots'][i]:
-                                st.pyplot(plot, clear_figure=True)
-                    
-                    if st.session_state['dataframes'][i]:
-                        for df in st.session_state['dataframes'][i]:
-                            st.dataframe(df)
-                    elif subscription_tier == "Free" and st.session_state['plots'][i]:
-                        st.warning("Upgrade to the Pro or Premium tier to view generated graphs and plots.")
-                        break
-            except Exception as e:
-                st.error(f"Error: {str(e)}")
-        del st.session_state['user_input']
-    # else:
-    #     if st.button('Send', key='send_button'):
-    #         st.session_state['user_input'] = user_input
-
-    if st.button('Ask', key='ask_button'):
-        st.session_state['user_input'] = user_input
+    # agent = create_pandas_dataframe_agent(chat, iris, verbose=True, allow_dangerous_code=True)
+    agent = create_pandas_dataframe_agent(chat, iris, return_intermediate_steps=True, save_charts=True, verbose=True,allow_dangerous_code=True)
+    # Set up the Streamlit app
+    st.title('Chatbot with Streamlit')
+    st.write("Ask a question to the chatbot")
+    imgs_png = glob.glob('*.png')
+    imgs_jpg = glob.glob('*.jpg')
+    imgs_jpeeg = glob.glob('*.jpeg')
+    imgs_ = imgs_png + imgs_jpg + imgs_jpeeg
+    if len(imgs_) > 0:
+        img = image_select("Generated Charts/Graphs", imgs_, captions =imgs_, return_value = 'index')
+        st.write(img)
+    # User input
+    # question = st.text_input("Ask Your question:")
+    x = 0
+    user_input = get_text(x)
+    if st.button('Ask'):
+        # if question:
+        #     # Get the response from the chatbot agent
+        #     try:
+        #         response = agent(question)
+        #         st.write(response['output'])
+        #     except Exception as e:
+        #         st.error(f"Error: {str(e)}")
+        # else:
+        #     st.write("Please enter a question.")
+        x+=1
+            #st.write("You:", user_input)
+        print(user_input, len(user_input))
+        response, thought, action, action_input, observation = run_query(agent, user_input)
+        #st.write("Pandas Agent: ")
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(response)
+        for i in range(len(st.session_state['generated'])-1, -1, -1):
+            message(st.session_state["generated"][i], key=str(i))
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+        for i in range(0, len(thought)):
+            st.sidebar.write(thought[i])
+            st.sidebar.write(action[i])
+            st.sidebar.write(action_input[i])
+            st.sidebar.write(observation[i])
+            st.sidebar.write('====')
 
 if __name__ == "__main__":
     if 'generated' not in st.session_state:
@@ -139,11 +83,8 @@ if __name__ == "__main__":
 
     if 'past' not in st.session_state:
         st.session_state['past'] = []
-
-    if 'plots' not in st.session_state:
-        st.session_state['plots'] = []
-
-    if 'dataframes' not in st.session_state:
-        st.session_state['dataframes'] = []
+    
+    if 'tabs' not in st.session_state:
+        st.session_state['tabs'] = []
 
     main()
--- streamlit.py
+++ streamlit.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 from dotenv import load_dotenv
@@ -7,37 +6,28 @@ from functions import *
 from langchain_community.chat_models import ChatOpenAI
 from langchain_experimental.agents.agent_toolkits.pandas.base import create_pandas_dataframe_agent
 from streamlit_chat import message
-import openai
-import sounddevice as sd
-import numpy as np
-import wavio
-from openai import OpenAI
-client = OpenAI()
+
 # Load environment variables from .env
 load_dotenv()
 
-# Initialize OpenAI API key
-api_key = os.getenv('OPENAI_API_KEY')
-if api_key:
-    openai.api_key = api_key
-else:
-    st.error("OpenAI API key is missing. Please set it in the .env file.")
-
-def record_audio():
-    duration = 3  # seconds
-    fs = 44100  # sample rate
-    st.info("Recording for 3 seconds...")
-
-    myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=2)
-    sd.wait()  # Wait until recording is finished
-
-    wavio.write("recording.wav", myrecording, fs, sampwidth=2)
-    return "recording.wav"
+def get_text(n):
+    input_text = st.text_input('Type your question here.', '', key="input{}".format(n))
+    return input_text
 
 def main():
-    # Load the preloaded dataset
-    preloaded_iris = pd.read_excel('Dummy Dataset for Challenge #1.xlsx', 'Database')
-    preloaded_iris = format_sales_figures(preloaded_iris)
+    # Access your API key
+    api_key = os.getenv('OPENAI_API_KEY')
+
+    if not api_key:
+        st.error("OpenAI API key is missing. Please set it in the .env file.")
+        return
+
+    # Load the dataset
+    iris = pd.read_excel('Dummy Dataset for Challenge #1.xlsx', 'Database')
+
+    # Create the chatbot agent
+    chat = ChatOpenAI(openai_api_key=api_key, model_name='gpt-3.5-turbo', temperature=0.0)
+    agent = create_pandas_dataframe_agent(chat, iris, return_intermediate_steps=True, verbose=True, allow_dangerous_code=True)
 
     # Set up the Streamlit app
     st.title('Welcome to ChatInsights')
@@ -49,52 +39,17 @@ def main():
     st.sidebar.write("- Access to basic chatbot features")
     st.sidebar.write("- Display of dataframes")
     st.sidebar.write("### Pro Tier:")
+    st.sidebar.write("- Access to all Free Tier features")
     st.sidebar.write("- Display of generated graphs and plots")
-    st.sidebar.write("### Premium Tier:")
-    st.sidebar.write("- Ability to upload your own datasets in place of the preloaded dataset")
 
     # Subscription selection
-    subscription_tier = st.sidebar.selectbox("Select your subscription tier:", ("Free", "Pro", "Premium"))
+    subscription_tier = st.sidebar.selectbox("Select your subscription tier:", ("Free", "Pro"))
 
     # User input
     x = 0
     user_input = get_text(x)
 
-    user_uploaded_dfs = []
-    if subscription_tier == "Premium":
-        st.title("Upload your datasets")
-        uploaded_files = st.file_uploader("Choose files", type=["csv", "xlsx", "xls"], accept_multiple_files=True)
-        for uploaded_file in uploaded_files:
-            try:
-                if uploaded_file.name.endswith(".csv"):
-                    df = load_csv_with_error_handling(uploaded_file)
-                else:
-                    df = pd.read_excel(uploaded_file)
-                if df is not None:
-                    df = format_sales_figures(df)
-                    user_uploaded_dfs.append(df)
-            except Exception as e:
-                st.error(f"An unexpected error occurred with file {uploaded_file.name}: {e}")
-                return
-
-    # Use user-uploaded datasets if available, otherwise use preloaded dataset
-    if user_uploaded_dfs:
-        combined_df = pd.concat(user_uploaded_dfs, ignore_index=True)
-    else:
-        combined_df = preloaded_iris
-
-    # Create the chatbot agent
-    chat = ChatOpenAI(openai_api_key=api_key, model_name='gpt-3.5-turbo', temperature=0.0)
-    agent = create_pandas_dataframe_agent(chat, combined_df, return_intermediate_steps=True, verbose=True, allow_dangerous_code=True)
-
-    if st.button('Record a question'):
-        audio_file_path = record_audio()
-        st.session_state['user_input'] = transcribe_audio(audio_file_path)
-    
-    
-    
-    if 'user_input' in st.session_state:
-        user_input = st.session_state['user_input']
+    if st.button('Ask'):
         if user_input:
             try:
                 response, thought, action, action_input, observation, plot_objects, dataframe_objects = run_query(agent, user_input)
@@ -107,27 +62,25 @@ def main():
                     message(st.session_state["generated"][i], key=str(i))
                     message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
 
-                    if subscription_tier in ["Pro", "Premium"]:
+                    if subscription_tier == "Pro":
                         if st.session_state['plots'][i]:
                             for plot in st.session_state['plots'][i]:
-                                st.pyplot(plot, clear_figure=True)
+                                st.pyplot(plot)
                     
                     if st.session_state['dataframes'][i]:
                         for df in st.session_state['dataframes'][i]:
                             st.dataframe(df)
-                    elif subscription_tier == "Free" and st.session_state['plots'][i]:
-                        st.warning("Upgrade to the Pro or Premium tier to view generated graphs and plots.")
-                        break
-                
-                audio_response = text_to_speech(response)
-                play_audio(audio_response)
 
+                # for i in range(len(thought)):
+                #     st.sidebar.write(f"Thought: {thought[i]}")
+                #     st.sidebar.write(f"Action: {action[i]}")
+                #     st.sidebar.write(f"Action Input: {action_input[i]}")
+                #     st.sidebar.write(f"Observation: {observation[i]}")
+                #     st.sidebar.write('====')
             except Exception as e:
                 st.error(f"Error: {str(e)}")
-        del st.session_state['user_input']
-
-    if st.button('Ask', key='ask_button'):
-        st.session_state['user_input'] = user_input
+        else:
+            st.write("Please enter a question.")
 
 if __name__ == "__main__":
     if 'generated' not in st.session_state:
